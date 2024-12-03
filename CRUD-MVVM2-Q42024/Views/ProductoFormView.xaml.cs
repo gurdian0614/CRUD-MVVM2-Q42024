8f7866f2421a1cd52bbc758b68f3d88da1431e1b
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-02 19:05:41 -0600

    Build view

--- CRUD-MVVM2-Q42024/Views/ProductoFormView.xaml.cs
+++ CRUD-MVVM2-Q42024/Views/ProductoFormView.xaml.cs
@@ -1,16 +1,22 @@
 using CRUD_MVVM2_Q42024.Models;
+using CRUD_MVVM2_Q42024.ViewModels;
 
 namespace CRUD_MVVM2_Q42024.Views;
 
 public partial class ProductoFormView : ContentPage
 {
+	ProductoFormViewModel viewModel = new ProductoFormViewModel();
 	public ProductoFormView()
 	{
 		InitializeComponent();
+		viewModel = new ProductoFormViewModel();
+		BindingContext = viewModel;
 	}
 
 	public ProductoFormView(Producto producto)
 	{
         InitializeComponent();
+        viewModel = new ProductoFormViewModel(producto);
+        BindingContext = viewModel;
     }
 }
