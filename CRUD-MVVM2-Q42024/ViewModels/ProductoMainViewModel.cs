3d249c381c11b0b9467ea8cfd23aa4872546003b
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-02 19:05:30 -0600

    Build view model

--- CRUD-MVVM2-Q42024/ViewModels/ProductoMainViewModel.cs
+++ CRUD-MVVM2-Q42024/ViewModels/ProductoMainViewModel.cs
@@ -1,7 +1,47 @@
 ﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using CRUD_MVVM2_Q42024.Models;
+using CRUD_MVVM2_Q42024.Services;
+using CRUD_MVVM2_Q42024.Views;
+using System.Collections.ObjectModel;
+
 namespace CRUD_MVVM2_Q42024.ViewModels
 {
-    public class ProductoMainViewModel
+    public partial class ProductoMainViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private ObservableCollection<Producto> productoCollection = new ObservableCollection<Producto>();
+
+        private readonly ProductoService service;
+
+        public ProductoMainViewModel()
+        {
+            service = new ProductoService();
+        }
+
+        private void Alerta(string Titulo, string Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Titulo, Mensaje, "Aceptar"));
+        }
+
+        public void GetAll()
+        {
+            var getAll = service.GetAll();
+
+            if (getAll.Count > 0)
+            {
+                ProductoCollection.Clear();
+                foreach (var producto in getAll) {
+                    ProductoCollection.Add(producto);
+                }
+            }
+        }
+
+        [RelayCommand]
+        private async Task GoToProductoFormView()
+        {
+            await App.Current!.MainPage!.Navigation.PushAsync(new ProductoFormView());
+        }
     }
 }
