450a56633b62aa7bf08f3d046fabb5b5f0a4dbe4
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-04 19:10:00 -0600

    Build view model

--- CRUD-MVVM2-Q42024/ViewModels/ProductoMainViewModel.cs
+++ CRUD-MVVM2-Q42024/ViewModels/ProductoMainViewModel.cs
@@ -5,6 +5,7 @@ using CRUD_MVVM2_Q42024.Models;
 using CRUD_MVVM2_Q42024.Services;
 using CRUD_MVVM2_Q42024.Views;
 using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
 
 namespace CRUD_MVVM2_Q42024.ViewModels
 {
@@ -42,6 +43,60 @@ namespace CRUD_MVVM2_Q42024.ViewModels
         private async Task GoToProductoFormView()
         {
             await App.Current!.MainPage!.Navigation.PushAsync(new ProductoFormView());
+        }
+
+        [RelayCommand]
+        private async Task GoToEditProductoFormView(Producto producto)
+        {
+            await App.Current!.MainPage!.Navigation.PushAsync(new ProductoFormView(producto));
+        }
+
+        private async Task EliminarProducto(Producto producto)
+        {
+            try
+            {
+                bool respuesta = await App.Current!.MainPage.DisplayAlert("ELIMINAR PRODUCTO", "¿Desea eliminar el producto?", "Si", "No");
+
+                if (respuesta)
+                {
+                    int del = service.Delete(producto);
+
+                    if (del > 0)
+                    {
+                        Alerta("ELIMINAR PRODUCTO", "Producto eliminado correctamente");
+                        ProductoCollection.Remove(producto);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Alerta("ERROR", ex.Message);
+            }
+        }
+
+        [RelayCommand]
+        private async Task SelectProducto(Producto producto)
+        {
+            try
+            {
+                const string ACTUALIZAR = "Actualizar";
+                const string ELIMINAR = "Eliminar";
+
+                string res = await App.Current!.MainPage!.DisplayActionSheet("OPCIONES", "Cancelar", null, ACTUALIZAR, ELIMINAR);
+
+                if (res == ACTUALIZAR)
+                {
+                    await GoToEditProductoFormView(producto);
+                }
+                else if (res == ELIMINAR)
+                {
+                    await EliminarProducto(producto);
+                }
+            }
+            catch (Exception ex)
+            {
+                Alerta("ERROR", ex.Message);
+            }
         }
     }
 }
