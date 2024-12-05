a90f01c377633509fc20d9975095b7c069bb8b1a
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-04 19:10:13 -0600

    Build main view

--- CRUD-MVVM2-Q42024/Views/ProductoMainView.xaml.cs
+++ CRUD-MVVM2-Q42024/Views/ProductoMainView.xaml.cs
@@ -1,9 +1,20 @@
+using CRUD_MVVM2_Q42024.ViewModels;
+
 namespace CRUD_MVVM2_Q42024.Views;
 
 public partial class ProductoMainView : ContentPage
 {
+	private ProductoMainViewModel viewModel;
 	public ProductoMainView()
 	{
 		InitializeComponent();
+		viewModel = new ProductoMainViewModel();
+		this.BindingContext = viewModel;
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+		viewModel.GetAll();
+    }
 }
