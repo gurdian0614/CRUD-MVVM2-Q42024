dacd61bd947036fae4bf977ddaaab5126de23966
gurdian0614 <ivan.gurdian@outlook.com>
2024-12-04 19:09:22 -0600

    Delete unnecessary files

--- CRUD-MVVM2-Q42024/Services/ProductoService.cs
+++ CRUD-MVVM2-Q42024/Services/ProductoService.cs
@@ -12,13 +12,6 @@ namespace CRUD_MVVM2_Q42024.Services
             string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Producto.db3");
             connection = new SQLiteConnection(dbPath);
             connection.CreateTable<Producto>();
-
-            Producto producto = new Producto();
-            producto.Codigo = "ABC1234";
-            producto.Nombre = "Coca cola 3 litros";
-            producto.Cantidad = 100;
-            producto.Precio = 76;
-            Insert(producto);
         }
 
         public List<Producto> GetAll()
